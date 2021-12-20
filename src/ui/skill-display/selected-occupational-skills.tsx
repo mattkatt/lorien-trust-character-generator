d1f8f3aecff6a97acb16d8f3f0760f00760b6a43
mattkatt <m4ttk4tt@hotmail.co.uk>
2021-12-20 02:12:08 +0000

    feat(SelectedOccupationalSkills): filter shown skills

--- src/ui/skill-display/selected-occupational-skills.tsx
+++ src/ui/skill-display/selected-occupational-skills.tsx
@@ -6,11 +6,17 @@ export const SelectedOccupationalSkills: FC = () => {
   const { characterState } = useContext(CharacterContext);
   const { occupationalSkills } = characterState;
 
+  const skillsToDisplay = occupationalSkills.map((skill) => {
+    const isOverwritten = occupationalSkills.some((s) => s.replaces === skill.id);
+
+    return !isOverwritten ? <li>{skill.name}</li> : null;
+  });
+
   return (
     <div style={{ backgroundColor: 'white', padding: '28px' }}>
       <h2>Selected Occupational Skills:</h2>
-      {occupationalSkills.length >= 0 ? <Alert message='No skills selected' /> : null}
-      <ul>{occupationalSkills.map((skill) => skill.name)}</ul>
+      {occupationalSkills.length <= 0 ? <Alert message='No skills selected' /> : null}
+      <ul>{skillsToDisplay}</ul>
     </div>
   );
 };

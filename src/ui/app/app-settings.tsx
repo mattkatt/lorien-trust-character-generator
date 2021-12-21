3271bf7cc1641c03be42aa386d00db7b2485892b
mattkatt <m4ttk4tt@hotmail.co.uk>
2021-12-21 01:03:09 +0000

    fix(AppSettings): useEffect to set displayed settings values correctly on app load

--- src/ui/app/app-settings.tsx
+++ src/ui/app/app-settings.tsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Checkbox } from 'antd';
 import { AppContext } from '../../context/app-context';
 
 export const AppSettings = () => {
   const { appState, setHideDisabledSkills } = useContext(AppContext);
+  const [hideDisabled, setHideDisabled] = useState(appState.hideDisabledSkills);
+
+  useEffect(() => {
+    setHideDisabled(appState.hideDisabledSkills);
+  }, [appState, setHideDisabled]);
 
   return (
     <>
       <h2>Settings</h2>
 
       <Checkbox
-        value={appState.hideDisabledSkills}
+        checked={hideDisabled}
         onClick={() => setHideDisabledSkills(!appState.hideDisabledSkills)}
       >
         Hide Disabled Skills
